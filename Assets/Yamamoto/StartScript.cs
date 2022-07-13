46876a3441c4f08bf73eaf7bd32f1dd2f6725ce8
Daichi Yamamoto <yamada.one.five@gmail.com>
2022-07-13 18:54:50 +0900

    スタートボタン/タイトル画面に別のパネル（チュートリアル）を表示させるボタンを作成

--- Assets/Yamamoto/StartScript.cs
+++ Assets/Yamamoto/StartScript.cs
@@ -1,18 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class StartScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	//　スタートボタンを押したら実行する
+	public void StartGame()
+	{
+		SceneManager.LoadScene("PlayScene");
+	}
 }
